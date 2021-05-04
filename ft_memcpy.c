1f2c3fb7b247b3b5aa667dbe76b2bab302910aab
mikastiv <mikastiv@outlook.com>
2021-05-04 17:31:22 -0400

    norminette

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/04 15:03:39 by mleblanc          #+#    #+#             */
-/*   Updated: 2021/05/04 15:07:52 by mleblanc         ###   ########.fr       */
+/*   Updated: 2021/05/04 17:29:37 by mleblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	unsigned char	*d;
-	unsigned char	*s;
+	unsigned char		*d;
+	const unsigned char	*s;
 
 	d = (unsigned char *)dest;
 	s = (unsigned char *)src;
