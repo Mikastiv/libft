5366daded90ded2a3427051c64ca52c456fa824f
mikastiv <mikastiv@outlook.com>
2021-05-06 16:05:07 -0400

    split

---
+++ ft_split.c
@@ -0,0 +1,119 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/06 15:27:20 by mleblanc          #+#    #+#             */
+/*   Updated: 2021/05/06 16:00:34 by mleblanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+#include <stdbool.h>
+
+static size_t	ft_count_words(const char *s, const char c)
+{
+	size_t		count;
+	const char	*w_start;
+	bool		in_word;
+
+	count = 0;
+	in_word = 0;
+	w_start = s;
+	while (*s)
+	{
+		if (in_word && *s == c)
+			count++;
+		if (*s == c)
+			in_word = false;
+		else
+			in_word = true;
+		if (!in_word)
+			w_start = s;
+		s++;
+	}
+	if (in_word)
+		count++;
+	return (count);
+}
+
+static char	*ft_get_word(const char *s, int len, char c)
+{
+	char	*word;
+
+	if (*s == c)
+	{
+		s++;
+		len--;
+	}
+	word = (char *)malloc((len + 1) * sizeof(char));
+	if (!word)
+		return (NULL);
+	ft_strlcpy(word, s, len + 1);
+	return (word);
+}
+
+static void	ft_append_words(char **arr, char const *s, char c)
+{
+	const char	*w_start;
+	int			i;
+	bool		in_word;
+
+	w_start = s;
+	in_word = false;
+	i = 0;
+	while (*s)
+	{
+		if (in_word && *s == c)
+			arr[i++] = ft_get_word(w_start, s - w_start, c);
+		if (*s == c)
+			in_word = false;
+		else
+			in_word = true;
+		if (!in_word)
+			w_start = s;
+		s++;
+	}
+	if (in_word)
+		arr[i++] = ft_get_word(w_start, s - w_start, c);
+	arr[i] = NULL;
+}
+
+static void	ft_free_str_array(char **arr, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (arr[i] != NULL)
+			free(arr[i]);
+		i++;
+	}
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**ret;
+	size_t	word_count;
+	size_t	i;
+
+	word_count = ft_count_words(s, c);
+	ret = (char **)malloc((word_count + 1) * sizeof(char *));
+	if (!ret)
+		return (NULL);
+	ft_append_words(ret, s, c);
+	i = 0;
+	while (i < word_count)
+	{
+		if (ret[i++] == NULL)
+		{
+			ft_free_str_array(ret, word_count);
+			return (NULL);
+		}
+	}
+	return (ret);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/04 12:39:54 by mleblanc          #+#    #+#             */
-/*   Updated: 2021/05/06 14:27:29 by mleblanc         ###   ########.fr       */
+/*   Updated: 2021/05/06 15:27:50 by mleblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,5 +46,6 @@ void	*ft_calloc(size_t count, size_t size);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 
 #endif
