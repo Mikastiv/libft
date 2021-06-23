d4ce03d841794cca3717baa5ea5139a9d60f016b
Mikastiv <mikastiv@outlook.com>
2021-06-22 20:09:30 -0400

    added free

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/04 12:39:54 by mleblanc          #+#    #+#             */
-/*   Updated: 2021/06/17 22:52:28 by mleblanc         ###   ########.fr       */
+/*   Updated: 2021/06/22 20:07:32 by mleblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
+void	ft_free(void **ptr);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 int		ft_isupper(int c);
