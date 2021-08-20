f7b8a7476427ebe3a75b608ba79a1fd536dc23fa
mleblanc <mleblanc@student.42quebec.com>
2021-08-20 16:57:56 -0400

    update join free

--- src/ft_string/ft_strjoin_free.c
+++ src/ft_string/ft_strjoin_free.c
@@ -6,7 +6,7 @@
 /*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/20 16:46:58 by mleblanc          #+#    #+#             */
-/*   Updated: 2021/08/20 16:50:33 by mleblanc         ###   ########.fr       */
+/*   Updated: 2021/08/20 16:57:38 by mleblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,11 @@ char	*ft_strjoin_free(char *s1, char *s2)
 	char	*str;
 
 	if (!s1 || !s2)
+	{
+		free(s1);
+		free(s2);
 		return (NULL);
+	}
 	str = ft_strjoin(s1, s2);
 	free(s1);
 	free(s2);
