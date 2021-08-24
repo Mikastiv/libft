7e4d6d235266381c2ebc090ecbcb2d197fea98f8
mleblanc <mleblanc@student.42quebec.com>
2021-08-24 16:54:10 -0400

    fix

--- src/ft_cstr/ft_strdup.c
+++ src/ft_cstr/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/05 20:34:07 by mleblanc          #+#    #+#             */
-/*   Updated: 2021/08/24 16:52:35 by mleblanc         ###   ########.fr       */
+/*   Updated: 2021/08/24 16:53:46 by mleblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ char	*ft_strdup(const char *s)
 	size_t	size;
 	char	*dup;
 
-	size = ft_strlen(s1) + 1;
-	s2 = ft_calloc(size, sizeof(char));
-	if (!s2)
+	size = ft_strlen(s) + 1;
+	dup = ft_calloc(size, sizeof(char));
+	if (!dup)
 		return (NULL);
 	ft_memcpy(dup, s, size * sizeof(char));
 	return (dup);
