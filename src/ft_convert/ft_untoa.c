4accdf6c64fdf6cc87be6d3c074a2a7a363769cc
mikastiv <m.leblanc_3@hotmail.com>
2021-08-25 00:14:18 -0400

    fix

--- src/ft_convert/ft_untoa.c
+++ src/ft_convert/ft_untoa.c
@@ -6,7 +6,7 @@
 /*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/07 18:45:15 by mleblanc          #+#    #+#             */
-/*   Updated: 2021/08/25 00:05:00 by mleblanc         ###   ########.fr       */
+/*   Updated: 2021/08/25 00:13:29 by mleblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 void	ft_untoa(char *b, unsigned long long n, size_t len)
 {
-	return (ft_untoa_base(b, n, len, "0123456789"));
+	ft_untoa_base(b, n, len, "0123456789");
 }
