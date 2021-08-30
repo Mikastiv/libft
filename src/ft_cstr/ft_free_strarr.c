9fa10e74895297d05e49471fa894debec1a5a743
mleblanc <mleblanc@student.42quebec.com>
2021-08-30 18:58:45 -0400

    check null

--- src/ft_cstr/ft_free_strarr.c
+++ src/ft_cstr/ft_free_strarr.c
@@ -6,7 +6,7 @@
 /*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/21 03:39:16 by mleblanc          #+#    #+#             */
-/*   Updated: 2021/08/21 03:40:17 by mleblanc         ###   ########.fr       */
+/*   Updated: 2021/08/30 18:54:59 by mleblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ft_free_strarr(char **arr)
 {
 	size_t	i;
 
+	if (!arr)
+		return ;
 	i = 0;
 	while (arr[i])
 		free(arr[i++]);
