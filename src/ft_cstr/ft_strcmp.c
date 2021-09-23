1dd6eeaa051b3b016042929de3a9ec77233065f5
mikastiv <m.leblanc_3@hotmail.com>
2021-09-23 11:17:44 -0400

    fix

--- src/ft_cstr/ft_strcmp.c
+++ src/ft_cstr/ft_strcmp.c
@@ -6,18 +6,18 @@
 /*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/22 05:11:05 by mleblanc          #+#    #+#             */
-/*   Updated: 2021/09/22 05:12:18 by mleblanc         ###   ########.fr       */
+/*   Updated: 2021/09/23 11:15:24 by mleblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
-	while (*s1)
+	while (*s1 == *s2)
 	{
-		if ((unsigned char)*s1 != (unsigned char)*s2)
-			return ((unsigned char)*s1 - (unsigned char)*s2);
+		if (*s1 == '\0')
+			return (0);
 		++s1;
 		++s2;
 	}
-	return (0);
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
