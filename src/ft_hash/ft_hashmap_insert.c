2cbd50b31c34cbb85367a4224f13096cc811edcd
mikastiv <m.leblanc_3@hotmail.com>
2021-09-28 16:49:56 -0400

    no duplicates

--- src/ft_hash/ft_hashmap_insert.c
+++ src/ft_hash/ft_hashmap_insert.c
@@ -6,7 +6,7 @@
 /*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 11:18:10 by mleblanc          #+#    #+#             */
-/*   Updated: 2021/09/28 12:54:29 by mleblanc         ###   ########.fr       */
+/*   Updated: 2021/09/28 16:33:05 by mleblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,36 @@
 #include "ft_hashmap.h"
 #include <stdlib.h>
 
+static t_pair	*create_pair(const char *key, void *value)
+{
+	t_pair	*pair;
+
+	pair = ft_calloc(1, sizeof(t_pair));
+	if (!pair)
+		return (NULL);
+	pair->key = ft_strdup(key);
+	if (!pair->key)
+	{
+		free(pair);
+		return (NULL);
+	}
+	pair->value = value;
+	return (pair);
+}
+
 bool	ft_hashmap_insert(t_hashmap m, const char *key, void *value)
 {
 	t_list		**ptr;
 	t_list		*new;
 	t_pair		*pair;
+	void		*exists;
 
-	pair = ft_calloc(1, sizeof(t_pair));
+	exists = ft_hashmap_find(m, key);
+	if (exists)
+		return (false);
+	pair = create_pair(key, value);
 	if (!pair)
 		return (false);
-	pair->key = ft_strdup(key);
-	if (!pair->key)
-	{
-		free(pair);
-		return (false);
-	}
-	pair->value = value;
 	new = ft_lstnew(pair);
 	if (!new)
 	{
