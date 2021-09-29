dc92704a706055a5ca18d8a43a5198b0b04d19d0
mikastiv <m.leblanc_3@hotmail.com>
2021-09-28 21:24:51 -0400

    added comments

--- src/ft_cstr/ft_strnstr.c
+++ src/ft_cstr/ft_strnstr.c
@@ -6,27 +6,27 @@
 /*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/05 15:48:07 by mleblanc          #+#    #+#             */
-/*   Updated: 2021/06/15 20:25:30 by mleblanc         ###   ########.fr       */
+/*   Updated: 2021/09/28 20:32:28 by mleblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t n)
 {
 	size_t	needle_len;
 
 	if (*needle == '\0')
 		return ((char *)haystack);
 	needle_len = ft_strlen(needle);
-	while (*haystack && len >= needle_len)
+	while (*haystack && n >= needle_len)
 	{
 		if (*haystack == *needle)
 		{
 			if (ft_strncmp(haystack, needle, needle_len) == 0)
 				return ((char *)haystack);
 		}
-		--len;
+		--n;
 		++haystack;
 	}
 	return (NULL);
