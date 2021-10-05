b2059e5943aa034e3d8ac027cdf5a1bf2f0088d7
mikastiv <m.leblanc_3@hotmail.com>
2021-10-04 23:14:24 -0400

    norme

--- src/ft_string/ft_str_add_back.c
+++ src/ft_string/ft_str_add_back.c
@@ -6,7 +6,7 @@
 /*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/23 16:01:22 by mleblanc          #+#    #+#             */
-/*   Updated: 2021/10/04 19:22:33 by mleblanc         ###   ########.fr       */
+/*   Updated: 2021/10/04 23:13:58 by mleblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ static bool	grow_buffer(t_string_ *str)
 
 	newsize = (size_t)((str->capacity + 1) * 1.5);
 	buf = ft_realloc(str->data,
-		str->capacity * sizeof(char), newsize * sizeof(char));
+			str->capacity * sizeof(char), newsize * sizeof(char));
 	if (!buf)
 		return (false);
 	str->data = buf;
